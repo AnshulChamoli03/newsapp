74428452afd83c9a31b616249d9b0199997cba32
AnshulChamoli03 <2021mmb1343.ac.in>
2023-06-23 13:51:52 +0530

    first commit

---
+++ src/compoments/News.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Newsitem from './Newsitem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        hi it is news
+        <Newsitem/>
+      </div>
+    )
+  }
+}
+
+export default News
---
+++ src/compoments/Newsitem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Newsitem extends Component {
+  render() {
+    return (
+      <div>
+        it is News item
+      </div>
+    )
+  }
+}
+
+export default Newsitem
+
